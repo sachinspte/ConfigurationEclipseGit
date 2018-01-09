334d55fe0aef62e3934f33506c950b24bede4093
sachin <sachinpate9@gmail.com>
2018-01-09 15:44:45 +0530

    Initial check of workspace

---
+++ EclipseGitconfiguration/src/demo/configuration/GitEclipseConfig.java
@@ -0,0 +1,12 @@
+package demo.configuration;
+
+public class GitEclipseConfig 
+{
+	public static void main(String[] args) 
+	{
+     int a=10;     
+     int b=20;    
+     int c=a+b;
+     System.out.println("sum of a & b="+c);
+	}
+}
