e70675bb5964592a856b004f0b79b568cea18496
sachin <sachinpate9@gmail.com>
2018-01-09 16:18:02 +0530

    First check in

--- EclipseGitconfiguration/src/demo/configuration/GitEclipseConfig.java
+++ EclipseGitconfiguration/src/demo/configuration/GitEclipseConfig.java
@@ -8,5 +8,7 @@ public class GitEclipseConfig
      int b=20;    
      int c=a+b;
      System.out.println("sum of a & b="+c);
+     System.out.println("value of a"+a);
+     System.out.println("value of b"+b);
 	}
 }
